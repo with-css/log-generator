659e85b5e615fc4ed38f63067a1f29ee2753e8b2
with-css <yongjemini@gmail.com>
2024-10-27 18:11:09 +0900

    ADD workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/log-generator",
+  reactStrictMode: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
